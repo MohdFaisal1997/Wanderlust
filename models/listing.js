ddef31e257969aa484f990af7a1ccf02c87e905f
MohdFaisal1997 <mohdfaisal199754.mf@gmail.com>
2024-05-09 15:30:29 +0530

    initial commit

---
+++ models/listing.js
@@ -0,0 +1,92 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         require: true,
+//     },
+//     description: String,
+//     image: {
+//         url: {
+//           type: String,
+//           required: true,
+//         },
+//         filename: {
+//           type: String,
+//           required: true,
+//         },
+//       },
+//     price: Number,
+//     location: String,
+//     country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        require: true,
+    },
+    description: String,
+    image: {
+        url: {
+          type: String,
+        },
+        filename: {
+          type: String,
+          required: true,
+        },
+      },
+    price: Number,
+    location: String,
+    country: String,
+});
+    
+    const Listing = mongoose.model("Listing", listingSchema);
+    module.exports = Listing;
+
+// module.exports = Listing;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         require: true,
+//     },
+//     description: String,
+//     image: {
+//         type:String,
+//         required:true,
+//       },
+//     price: Number,
+//     location: String,
+//     country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//     title: {
+//         type: String,
+//         require: true,
+//     },
+//     description: String,
+//     image: {
+//         type:String,
+//         required: true,
+//       },
+//     price: Number,
+//     location: String,
+//     country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
